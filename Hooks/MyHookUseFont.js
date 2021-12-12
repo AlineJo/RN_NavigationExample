6bd2126e43778759cb70268bd6c14aa47a4649c8
AlineJo <josephali1993@gmail.com>
2021-12-12 14:56:33 +0400

    # Fix font is  not loading bug!

--- Hooks/MyHookUseFont.js
+++ Hooks/MyHookUseFont.js
@@ -1,12 +1,16 @@
-import Font from 'expo-font'
+import * as Font from 'expo-font'
 
+ 
 
-const fetchMyFonts=()=>{
-   return
-    Font.loadAsync({
-        'font-roboto_thin': require('../assets/fonts/Roboto-Thin.tff'),
-        'font-roboto_light': require('../assets/fonts/Roboto-Light.tff'),
-        'font-roboto_bold': require('../assets/fonts/Roboto-Bold.tff'),
+export const FONT_ROBOTO_THIN = 'font-roboto_thin'
+export const FONT_ROBOTO_LIGHT = 'font-roboto_light'
+export const FONT_ROBOTO_BOLD = 'font-roboto_bold'
+
+const fetchMyFonts = () => {
+    return Font.loadAsync({
+        'font-roboto_thin': require('../assets/fonts/Roboto-Thin.ttf'),
+        'font-roboto_light': require('../assets/fonts/Roboto-Light.ttf'),
+        'font-roboto_bold': require('../assets/fonts/Roboto-Bold.ttf'),
     })
 }
 
